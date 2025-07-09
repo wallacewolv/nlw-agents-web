f4a7300a47b0bc6e49cfa73ad4e994b53d44d032
Wallace W <wallace_wolv@hotmail.com>
2025-07-09 19:31:26 -0300

    feat: Implementando a listagem de salas na página CreateRoom

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,13 +1,35 @@
+import { useQuery } from '@tanstack/react-query'
 import { Link } from 'react-router-dom'
 
+type getRoomsAPIResponse = Array<{
+  id: string
+  name: string
+}>
+
 export function CreateRoom() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['get-rooms'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3333/rooms')
+      const result: getRoomsAPIResponse = await response.json()
+
+      return result
+    },
+  })
+
   return (
     <div>
-      <div>Create Room</div>
+      {isLoading && <span>Carregando...</span>}
 
-      <Link className="underline" to="/room">
-        Acessar sala
-      </Link>
+      <div className="flex flex-col gap-1">
+        {data?.map((room) => {
+          return (
+            <Link key={room.id} to={`/room/${room.id}`}>
+              {room.name}
+            </Link>
+          )
+        })}
+      </div>
     </div>
   )
 }
