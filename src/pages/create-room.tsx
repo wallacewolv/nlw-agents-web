305aebb1218e26d16ea4a33a04e4963ec1e4b771
Wallace W <wallace_wolv@hotmail.com>
2025-07-10 01:05:51 -0300

    feat: Implementando listagem de salas recentes

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,34 +1,14 @@
-import { useQuery } from '@tanstack/react-query'
-import { Link } from 'react-router-dom'
-
-type getRoomsAPIResponse = Array<{
-  id: string
-  name: string
-}>
+import { RoomList } from '@/components/room-list'
 
 export function CreateRoom() {
-  const { data, isLoading } = useQuery({
-    queryKey: ['get-rooms'],
-    queryFn: async () => {
-      const response = await fetch('http://localhost:3333/rooms')
-      const result: getRoomsAPIResponse = await response.json()
-
-      return result
-    },
-  })
-
   return (
-    <div>
-      {isLoading && <span>Carregando...</span>}
+    <div className="min-h-screen px-4 py-8">
+      <div className="mx-auto max-w-4xl">
+        <div className="grid grid-cols-2 items-start gap-8">
+          <div />
 
-      <div className="flex flex-col gap-1">
-        {data?.map((room) => {
-          return (
-            <Link key={room.id} to={`/room/${room.id}`}>
-              {room.name}
-            </Link>
-          )
-        })}
+          <RoomList />
+        </div>
       </div>
     </div>
   )
